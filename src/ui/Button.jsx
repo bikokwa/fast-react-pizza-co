ab9fba6d226cfe4854333802d4cafed887962f45
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-02-10 20:30:34 +0300

    style the cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,6 +6,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 sm:px-5 sm:py-2.5 text-xs",
+    secondary:
+      "inline-block rounded-full border-2 border-stone-300 font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
   if (to)
     return (
